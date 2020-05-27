274ed50f5199f8e08b33023558dc5c6294f56524
github tsubasashimazaki <github>
2020-05-27 15:07:22 +0900

    test.bladeファイルにコレクション型の練習

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -13,9 +13,15 @@ class TestController extends Controller
     {   
         // データベースからデータを表示させる
         $values = Test::all();
+        // collectでグループ分け
+        $collection = collect([1, 2, 3, 4, 5, 6, 7]);
+        // 一つのグループに4つ入れてね
+        $chunks = $collection->chunk(4);
+        // コレクションをPHPの配列に変換
+        $chunks->toArray();
 
         // この処理で止めて変数の中身を表示
-        //dd($values);
+        dd($chunks);
 
         // ビューはresources/viewsディレクトリのサブディレクトリにネスト。
         // ネストしたビューを参照するために「ドット」記法が使える
